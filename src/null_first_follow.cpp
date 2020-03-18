4c0f23c9d86a2f307b63e87c5c34fb9d57ecd237
Miles President <mcprez10@gmail.com>
2020-03-17 21:07:07 -0700

    Writing nullability.

--- src/null_first_follow.cpp
+++ src/null_first_follow.cpp
@@ -3,10 +3,38 @@
 #include <iostream>
 #include <cstddef>
 #include <string>
+#include <bitset>
 
 using namespace std;
 
 
-vector<Symbol> conjunction(const Rule& rule){
-  return {};
+using BitSet = std::bitset<static_cast<int>(Symbol::STARTTOKENS)>;
+using BitSetRef = BitSet::reference;
+
+/* Returns vector of pointers to nullability values of symbols in the rule.
+ * If nullability of rule is always true or always false, returns an empty
+ * vector and sets always accordingly. */
+vector<BitSetRef> conjunction(const Rule& rule, BitSet nullabilities, bool* always){
+  vector<BitSetRef> result;
+  for (const Symbol& symbol : rule.rhs) {
+    // Tokens are never nullable
+    if (isToken(symbol)) {
+      *always = false;
+      return {};
+    }
+
+    // Epsilon is always nullable
+    if (symbol == Symbol::EPSILON) {
+      *always = true;
+      return {};
+    }
+
+    result.push_back(nullabilities[static_cast<int>(symbol)]);
+  }
+  return result;
+}
+
+
+void initNull() {
+  BitSet nullabilities;
 }
