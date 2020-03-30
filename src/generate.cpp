6288bd5b37d55d10777e92f80d8936c9371d9c39
Miles President <mcprez10@gmail.com>
2020-03-30 06:48:15 -0700

    Code generation for grammar data.

--- src/generate.cpp
+++ src/generate.cpp
@@ -12,56 +12,132 @@
 using namespace std;
 
 
-string toCode(const string& str) {
-  return "\"" + str + "\"";
+void toCode(ostream& out, const string& str) {
+  out << '"' << str << '"';
 }
 
-string toCode(int n) {
-  return to_string(n);
+void toCode(ostream& out, int n) {
+  out << to_string(n);
+}
+
+void toCode(ostream& out, const Assoc& assoc) {
+  out << "Assoc::" << assoc;
 }
 
 template <typename Container>
-string& toCode(const Container& c) {
-  stringstream out;
+void toCode(ostream& out, const Container& c) {
   out << '{';
   for_each(c.cbegin(), c.cend(), [&out](auto item) {
-        item << toCode(n) << ',';
+        toCode(out, item);
+        out << ',';
       });
   out << '}';
 }
 
 
-string toCode(const Token& token) {
-
+void toCode(ostream& out,const Token& token) {
+  out << '{';
+  toCode(out, token.name);
+  out << ',';
+  toCode(out, token.precedence);
+  out << '}';
+  // Other fields not needed for shift-reducing
 }
 
-string toCode(const Concrete& concrete) {
-
+void toCode(ostream& out,const Concrete& concrete) {
+  out << '{';
+  toCode(out, concrete.varType);
+  out << '}';
+  // Other fields not needed for shift-reducing
 }
 
-string toCode(const Variable& variable) {
-
+void toCode(ostream& out,const GrammarData& grammarData) {
+  out << '{';
+  toCode(out, grammarData.tokens);
+  out << ',';
+  toCode(out, grammarData.concretes);
+  out << '}';
+  // Variables not needed for shift reducing
 }
 
-string toCode(const GrammarData& grammarData) {
 
+void tokenDecl(ostream& out) {
+  out << R"(struct Token {
+    string name;
+    int precedence;
+  };
+  )";
+}
+
+void concreteDecl(ostream& out) {
+  out << R"(struct Concrete {
+    int varType;
+  };
+  )";
+}
+
+void grammarDataDecl(ostream& out, const GrammarData& grammarData) {
+  out << R"(struct GrammarData {
+    vector<Token> tokens;
+    vector<Concrete> concretes;
+  };
+  )";
+  out << "GrammarData GRAMMAR_DATA = ";
+  toCode(out, grammarData);
+  out << ';';
 }
 
 
-// void generateParserDFA() {
-//   std::ofstream outFile;
-//   outFile.open("test/gen_parser.hpp");
-//   condensedDFAToCode(outFile, GRAMMAR_DATA);
-//   outFile.close();
-// }
+void includes(ostream& out) {
+  out << R"(#include <vector>
+    #include <string>
+    #include <iostream>
+    #include <cstddef>
+    #include <algorithm>
+    #include <functional>
+    #include <optional>
+    #include <stdexcept>
+  )";
+}
+
+
+/********************
+ * Driver Functions *
+ ********************/
+string hppCode() {
+  stringstream out;
+
+  includes(out);
+
+  return out.str();
+}
+
+string cppCode(const GrammarData& grammarData) {
+  stringstream out;
+
+  out << R"(#include "parser.hpp"
+    using namespace std;
+  )";
+  tokenDecl(out);
+  concreteDecl(out);
+  grammarDataDecl(out, grammarData);
+
+
+  // condensedDFAToCode(outFile, GRAMMAR_DATA);
+  return out.str();
+}
 
 
 void generateCode(const GrammarData& grammarData) {
-  std::ofstream outFile;
-  outFile.open("parser.cpp");
+  std::ofstream hppFile;
+  hppFile.open("parser.hpp");
+  hppFile << hppCode();
+  hppFile.close();
 
-
-  outFile.close();
+  std::ofstream cppFile;
+  cppFile.open("parser.cpp");
+  cppFile << cppCode(grammarData);
+  cppFile.close();
 }
 
 
